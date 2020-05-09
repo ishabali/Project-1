e2a44d7c1998033f36aba80400b38149b729286e
Gentry Hunt <ghunt@csu.edu>
2020-05-09 14:07:33 -0500

    queried for location in trip advisor, created variables for input locations. 1 error present

--- project.js
+++ project.js
@@ -1,42 +1,63 @@
 $(document).ready(function () {
     var location = '';
-    var APIKey = "b212266a3b5800f1c727bf9539b273bb";
-   // $('#go-weather').on('click', function () {
+    $('#go-weather').on('click', function (event) {
+        event.preventDefault();
         location = $("#len1").val();
-        alert(location);
-   //})
-    //Location//
-    var getData = function() {
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=bujumbura,burundi&appid=" + APIKey;
+        getWeatherData(location);
+        getTripAdvisorData(location);
+    })
+    var getWeatherData = function (location) {
+        var APIKey = "b212266a3b5800f1c727bf9539b273bb";
+        location = $('#len1').val();
+        location = 'Chicago';
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}`;
 
-        // We then created an AJAX call
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            $('.weather-api').append('<br>' + response.name);
-        })
+            var cityName = response.name;
+            $('.weather-api').append('<br>' + cityName);
 
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://tripadvisor1.p.rapidapi.com/photos/list?lang=en_US&currency=USD&limit=50&location_id=2233968",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-                "x-rapidapi-key": "363c7fbecfmshcbe4cfd3ee41b5fp142feajsn760531d33ab1"
-            }
-        }
-        $.ajax(settings).done(function (response) {
-            console.log(response.data);
-            $('.trip-advisor-api').append('<br>' + response.data[0].caption);
-        });
+        })
+        //------------------------------------------------------------------
+
+var getTripAdvisorData = function(location){
+    var settings = {
+
+        //endpoint: locations/search
+                    "async": true,
+                    "crossDomain": true,
+                    "url": `https://tripadvisor1.p.rapidapi.com/locations/search?location_
+                    id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${location}`,
+        
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+                        "x-rapidapi-key": "83c24764bamsh4ab0e0175aae739p1180b7jsn62d3645b374e"
+                    }
+                }
+                $.ajax(settings).done(function (response) {
+                    for (var i = 0; i < response.data.length; i++) {
+                        if (response.data[i].result_type === "geos" && response.data[i].result_object.name === cityName) {
+                            console.log(response.data[i].result_object.name);
+                            console.log(response.data[i].result_object.name);
+                            var city2 = response.data[i].result_object.name;
+                        }
+                    }
+        
+        
+        
+                      //$('.trip-advisor-api').append(`<br><img src="${imageURL}" class="img-fluid" alt="Responsive image"></img width="${imageWidth}">`);
+                });
+}
     }
     //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
     //trip advisor
 
-        getData(APIKey, location);
+
 
 })
 
